18b5332147167cc4e479c23093921a514769c7cf
DevNap <thdwodh789@gmail.com>
2025-05-14 11:35:16 +0900

    [Feat] PlayerStatus

---
+++ XRP102_SimpleTPS/Assets/Scripts/Player/PlayerStatus.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DesignPattern;
+
+public class PlayerStatus : MonoBehaviour
+{
+    [field: SerializeField]
+    [field: Range(0, 10)]
+
+    public float WalkSpeed { get; set; }
+
+    [field: SerializeField]
+    [field: Range(0, 10)]
+
+    public float RunSpeed { get; set; }
+
+    [field: SerializeField]
+    [field: Range(0, 10)]
+
+    public float RotateSpeed { get; set; }
+
+    public ObservableProperty<bool> ISAiming { get; private set; } = new();
+    public ObservableProperty<bool> ISMoving { get; private set; } = new();
+    public ObservableProperty<bool> ISAttacking { get; private set; } = new();
+}
